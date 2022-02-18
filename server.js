f32bfa42fc27d5dedc70b4a127e82be7c350f3fc
David Aikman <57766110+daikman@users.noreply.github.com>
2022-02-18 13:36:09 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        actions: 'get_board/<board_name>, check_board/<board_name>, save_board'
+    });
+});
+
+app.get('/get_board/:board_id', (req, res) => {
+    let board = req.params.board_id;
+
+    fs.readFile( __dirname + "/" + "adventures.json", 'utf8', function (err, data) {
+      var allAdventures = JSON.parse(data);
+      var adventures = allAdventures[board]
+      res.end( JSON.stringify(adventures));
+    });
+});
+
+app.get('/check_board/:board_id', (req, res) => {
+    let board = req.params.board_id;
+
+    fs.readFile( __dirname + "/" + "adventures.json", 'utf8', function (err, data) {
+
+      var allAdventures = JSON.parse(data);
+      let names = Object.keys(allAdventures)
+
+      let conflict
+      if (names.indexOf(board) == -1) {
+        conflict = false
+      } else {
+        conflict = true
+      }
+
+      res.end( JSON.stringify(
+        {
+          name_taken: conflict
+        }
+      ));
+
+    });
+
+});
+
+app.post('/save_board', function (req, res) {
+
+  // get old adventures
+  fs.readFile( __dirname + "/" + "adventures.json", 'utf8', function (err, data) {
+    data = JSON.parse( data );
+    // combine with new data
+    data = {...data, ...req.body}
+
+    // return as a response
+    res.end( JSON.stringify(data));
+
+    // write over file
+    fs.writeFile('adventures.json', JSON.stringify(data), (err) => {
+      if (err) {
+          throw err;
+      }
+      console.log("JSON data is saved.");
+    });
+  });
+
+})
+
+app.listen(2020, () => {
+    console.log('server is listening on port 2020');
+});
